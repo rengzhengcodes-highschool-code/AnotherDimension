4bd9eaeaadc4ad4ab8c3add55fe9bbd9cae06caa
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-23 13:50:20 -0400

    Implemented largest function and its related tester. Added comments about test array formatting.

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,10 +2,22 @@ public class ArrayOps {
 
   public static int sum(int[] arr) {
     int total = 0;
-    for (int index = 0;index < arr.length ; index++) {
+    for (int index = 0; index < arr.length; index++) {
       total += arr[index];
     }
     return total;
   }
+
+  public static  int largest(int[] arr) {
+    int largest = arr[0];
+
+    for (int index = 1; index < arr.length ; index++) {
+      if (arr[index] > largest) {
+        largest = arr[index];
+      }
+    }
+
+    return largest;
+ }
 
 }
--- Tester.java
+++ Tester.java
@@ -2,15 +2,17 @@ public class Tester {
 
   public static void main(String[] args) {
     sumTester();
+    largestTester();
   }
 
   public static void sumTester() {
-    int[][][] sumTestArrsAndAnswer = {
+    int[][][] sumTestArrsAndAnswer = {                                          //formatting is {testArray, {answer}}
       {{}, {0}},
       {{0}, {0}},
       {{1, 2, 3, 4, 5}, {15}},
       {{-1, 1, 3}, {3}},
     };
+
     for (int test = 0;test < sumTestArrsAndAnswer.length ; test++) {
       if (ArrayOps.sum(sumTestArrsAndAnswer[test][0]) == sumTestArrsAndAnswer[test][1][0]) {
         System.out.println("sum function passed test at index: " + test);
@@ -19,4 +21,25 @@ public class Tester {
       }
     }
   }
+
+  public static void largestTester() {
+    int[][][] largestTestArrsAndAnswer = {                                      //formatting is {testArray, {answer}}
+      {{0}, {0}},
+      {{-1}, {-1}},
+      {{1}, {1}},
+      {{0, -2, -1}, {0}},
+      {{1, 2, 3, 4, 5}, {5}},
+      {{-1, 1, 3}, {3}},
+      {{-1, -2, -5, 0}, {0}}
+    };
+
+    for (int test = 0;test < largestTestArrsAndAnswer.length ; test++) {
+      if (ArrayOps.largest(largestTestArrsAndAnswer[test][0]) == largestTestArrsAndAnswer[test][1][0]) {
+        System.out.println("largest function passed test at index: " + test);
+      } else {
+        System.out.println("largest function failed test at index: " + test);
+      }
+    }
+  }
+
 }
